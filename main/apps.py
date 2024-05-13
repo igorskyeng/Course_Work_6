7972aabf933a5cb0f5de82bdb788535156e75a9d
Igor Barbur <Igor.barbur@yandex.ru>
2024-05-13 19:27:32 +0800

    Adding a team to the automatic mailing list.

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,12 @@
+from time import sleep
 from django.apps import AppConfig
+from django.core.management import call_command
 
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self):
+        sleep(2)
+        call_command("start_mailing")
